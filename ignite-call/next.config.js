ee8c5bd0d5797ef1f14728e548a8c89f068ac03d
Lucas Schiavini <lucas.s.schiavini@gmail.com>
2023-01-17 10:42:45 -0300

    changes names of routes to have .page

--- ignite-call/next.config.js
+++ ignite-call/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  pageExtensions: [
+    'page.tsx', 'api.ts', 'api.tsx' // creates routes only if the file follows {{name}}.page.tsx
+  ]
 }
 
 module.exports = nextConfig
