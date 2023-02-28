16734efe12aa5fd53f1a4e0f704b7e82086f3dc0
Lucas Schiavini <lucas.s.schiavini@gmail.com>
2023-02-28 12:28:23 -0300

    section2-epi4

--- ignite-call/src/pages/home/components/ClaimUsernameForm/index.tsx
+++ ignite-call/src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,28 +1,48 @@
-import { Button, TextInput } from '@ignite-ui/react'
+import { Button, Text, TextInput } from '@ignite-ui/react'
 import { ArrowRight } from 'phosphor-react'
-import { Form } from './styles'
+import { Form, FormAnnotation } from './styles'
 import { useForm } from 'react-hook-form'
-import { ClaimUsernameFormData } from '@/src/pages/home/components/ClaimUsernameForm/schema'
+import {
+  ClaimUsernameFormData,
+  ClaimUsernameFormSchema,
+} from '@/src/pages/home/components/ClaimUsernameForm/schema'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 export function ClaimUsernameForm() {
-  const { register, handleSubmit } = useForm<ClaimUsernameFormData>()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ClaimUsernameFormData>({
+    resolver: zodResolver(ClaimUsernameFormSchema),
+  })
 
   async function handleClaimUsername(data: ClaimUsernameFormData) {
     console.log(data)
   }
 
+  console.log({ errors })
   return (
-    <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
-      <TextInput
-        size="sm"
-        prefix="ignite.com/"
-        {...register('username')}
-        placeholder="your-username"
-      />
-      <Button size="sm" type="submit">
-        Reserve
-        <ArrowRight />
-      </Button>
-    </Form>
+    <>
+      <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+        <TextInput
+          size="sm"
+          prefix="ignite.com/"
+          {...register('username')}
+          placeholder="your-username"
+        />
+        <Button size="sm" type="submit">
+          Reserve
+          <ArrowRight />
+        </Button>
+      </Form>
+      <FormAnnotation>
+        <Text size={'sm'}>
+          {errors.username
+            ? errors.username.message
+            : 'Type in the desired username'}
+        </Text>
+      </FormAnnotation>
+    </>
   )
 }
--- ignite-call/src/pages/home/components/ClaimUsernameForm/schema.ts
+++ ignite-call/src/pages/home/components/ClaimUsernameForm/schema.ts
@@ -1,10 +1,15 @@
 import { z } from 'zod'
 
 const ClaimUsernameFormSchema = z.object({
-  username: z.string(),
+  username: z
+    .string()
+    .min(3, { message: 'username needs to have at least 3 letters.' })
+    .regex(/^([a-z\\-]+)$/i, {
+      message: 'Must consist of letters and /or -',
+    }),
 })
 
 type ClaimUsernameFormData = z.infer<typeof ClaimUsernameFormSchema>
 
 export type { ClaimUsernameFormData }
-export default ClaimUsernameFormSchema
+export { ClaimUsernameFormSchema }
