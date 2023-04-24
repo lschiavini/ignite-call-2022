98c8c6cee1b3f52e2758103044d33d879764b74f
Lucas Schiavini <lucas.s.schiavini@gmail.com>
2023-04-24 11:05:39 -0300

    section 4 - init connect calendar with daytimes

---
+++ ignite-call/src/pages/register/time-intervals/index.page.tsx
@@ -0,0 +1,62 @@
+import {
+  Button,
+  Checkbox,
+  Heading,
+  MultiStep,
+  Text,
+  TextInput,
+} from '@ignite-ui/react'
+import { ArrowRight } from 'phosphor-react'
+import { Container, Header } from '../styles'
+
+import {
+  IntervalBox,
+  IntervalContainer,
+  IntervalDay,
+  IntervalInputs,
+  IntervalItem,
+} from './styles'
+
+export default function TimeIntervals() {
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Almost There!</Heading>
+        <Text>{"Tell us the time you're available each day of the week"}</Text>
+
+        <MultiStep size={4} currentStep={3} />
+      </Header>
+
+      <IntervalBox as="form">
+        <IntervalContainer>
+          <IntervalItem>
+            <IntervalDay>
+              <Checkbox />
+              <Text>Monday</Text>
+            </IntervalDay>
+            <IntervalInputs>
+              <TextInput size="sm" type="time" step={60} />
+              <TextInput size="sm" type="time" step={60} />
+            </IntervalInputs>
+          </IntervalItem>
+
+          <IntervalItem>
+            <IntervalDay>
+              <Checkbox />
+              <Text>Tuesday</Text>
+            </IntervalDay>
+            <IntervalInputs>
+              <TextInput size="sm" type="time" step={60} />
+              <TextInput size="sm" type="time" step={60} />
+            </IntervalInputs>
+          </IntervalItem>
+        </IntervalContainer>
+
+        <Button type="submit">
+          Next Step
+          <ArrowRight />
+        </Button>
+      </IntervalBox>
+    </Container>
+  )
+}
