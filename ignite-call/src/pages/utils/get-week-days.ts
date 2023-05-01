5e86e4a7358af8fd52cc4c683e65612cf40bf903
Lucas Schiavini <lucas.s.schiavini@gmail.com>
2023-05-01 18:32:53 -0300

    starting calendar screen

--- ignite-call/src/pages/utils/get-week-days.ts
+++ ignite-call/src/pages/utils/get-week-days.ts
@@ -1,4 +1,8 @@
-export function getWeekDays() {
+interface GetWeekDaysParams {
+  short?: boolean
+}
+
+export function getWeekDays({ short = false }: GetWeekDaysParams) {
   const formatter = new Intl.DateTimeFormat('en-US', { weekday: 'long' })
 
   return Array.from(Array(7).keys())
@@ -6,6 +10,9 @@ export function getWeekDays() {
       formatter.format(new Date(Date.UTC(new Date().getFullYear(), 7, day))),
     )
     .map((weekDay) => {
+      if (short) {
+        return weekDay.substring(0, 3).toUpperCase()
+      }
       return weekDay.substring(0, 1).toUpperCase().concat(weekDay.substring(1))
     })
 }
