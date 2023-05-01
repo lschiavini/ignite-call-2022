ef8d68ce2036b34d8e7ab6b55271a32f581dbfaa
Lucas Schiavini <lucas.s.schiavini@gmail.com>
2023-05-01 15:33:01 -0300

    Section 4 - form with days

--- ignite-call/src/pages/utils/get-week-days.ts
+++ ignite-call/src/pages/utils/get-week-days.ts
@@ -2,7 +2,9 @@ export function getWeekDays() {
   const formatter = new Intl.DateTimeFormat('en-US', { weekday: 'long' })
 
   return Array.from(Array(7).keys())
-    .map((day) => formatter.format(new Date(Date.UTC(2021, 5, day))))
+    .map((day) =>
+      formatter.format(new Date(Date.UTC(new Date().getFullYear(), 1, day))),
+    )
     .map((weekDay) => {
       return weekDay.substring(0, 1).toUpperCase().concat(weekDay.substring(1))
     })
