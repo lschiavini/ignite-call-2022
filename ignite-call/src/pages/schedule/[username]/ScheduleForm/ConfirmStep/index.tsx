f3a3536d4dbd14bd3d504ee94428fa4e3baaadff
Lucas Schiavini <lucas.s.schiavini@gmail.com>
2023-05-02 10:58:32 -0300

    confirm screen styling

---
+++ ignite-call/src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
@@ -0,0 +1,44 @@
+import { Button, Text, TextArea, TextInput } from '@ignite-ui/react'
+import { CalendarBlank, Clock } from 'phosphor-react'
+import { ConfirmForm, FormActions, FormHeader } from './styles'
+
+export function ConfirmStep() {
+  function handleConfirmScheduling() {}
+
+  return (
+    <ConfirmForm as="form" onSubmit={handleConfirmScheduling}>
+      <FormHeader>
+        <Text>
+          <CalendarBlank />
+          September 22th, 2023
+        </Text>
+        <Text>
+          <Clock />
+          18:00h
+        </Text>
+      </FormHeader>
+
+      <label>
+        <Text size="sm">Full name</Text>
+        <TextInput placeholder="Your name" />
+      </label>
+
+      <label>
+        <Text size="sm">e-mail</Text>
+        <TextInput type="email" placeholder="johndoe@example.com" />
+      </label>
+
+      <label>
+        <Text size="sm">Observations</Text>
+        <TextArea />
+      </label>
+
+      <FormActions>
+        <Button type="button" variant="tertiary">
+          Cancel
+        </Button>
+        <Button type="submit">Confirm</Button>
+      </FormActions>
+    </ConfirmForm>
+  )
+}
--- ignite-call/src/pages/schedule/[username]/ScheduleForm/index.tsx
+++ ignite-call/src/pages/schedule/[username]/ScheduleForm/index.tsx
@@ -1,5 +1,7 @@
-import { CalendarStep } from './CalendarStep'
+// import { CalendarStep } from './CalendarStep'
+import { ConfirmStep } from '@/src/pages/schedule/[username]/ScheduleForm/ConfirmStep'
 
 export function ScheduleForm() {
-  return <CalendarStep />
+  // return <CalendarStep />
+  return <ConfirmStep />
 }
