8a9a8e55125a64a37cd96556c614fd0ebf019bb3
Lucas Schiavini <lucas.s.schiavini@gmail.com>
2023-05-14 15:50:11 -0300

    confirmation flow

--- ignite-call/src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
+++ ignite-call/src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
@@ -4,6 +4,7 @@ import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 import { ConfirmForm, FormActions, FormError, FormHeader } from './styles'
 import { zodResolver } from '@hookform/resolvers/zod'
+import dayjs from 'dayjs'
 
 const confirmFormSchema = z.object({
   name: z
@@ -15,7 +16,15 @@ const confirmFormSchema = z.object({
 
 type ConfirmFormData = z.infer<typeof confirmFormSchema>
 
-export function ConfirmStep() {
+interface ConfirmStepProps {
+  schedulingDate: Date
+  onCancelConfirmation: () => void
+}
+
+export function ConfirmStep({
+  schedulingDate,
+  onCancelConfirmation,
+}: ConfirmStepProps) {
   const {
     register,
     handleSubmit,
@@ -24,16 +33,19 @@ export function ConfirmStep() {
 
   function handleConfirmScheduling() {}
 
+  const describedDate = dayjs(schedulingDate).format('DD[ de ]MMMM[ de ]YYYY')
+  const describedTime = dayjs(schedulingDate).format('HH:mm[h]')
+
   return (
     <ConfirmForm as="form" onSubmit={handleSubmit(handleConfirmScheduling)}>
       <FormHeader>
         <Text>
           <CalendarBlank />
-          September 22th, 2023
+          {describedDate}
         </Text>
         <Text>
           <Clock />
-          18:00h
+          {describedTime}
         </Text>
       </FormHeader>
 
@@ -63,7 +75,7 @@ export function ConfirmStep() {
       </label>
 
       <FormActions>
-        <Button type="button" variant="tertiary">
+        <Button type="button" variant="tertiary" onClick={onCancelConfirmation}>
           Cancel
         </Button>
         <Button type="submit" disabled={isSubmitting}>
--- ignite-call/src/pages/schedule/[username]/ScheduleForm/index.tsx
+++ ignite-call/src/pages/schedule/[username]/ScheduleForm/index.tsx
@@ -1,8 +1,22 @@
 import { CalendarStep } from './CalendarStep'
-
-// import { ConfirmStep } from '@/src/pages/schedule/[username]/ScheduleForm/ConfirmStep'
+import { ConfirmStep } from '@/src/pages/schedule/[username]/ScheduleForm/ConfirmStep'
+import { useState } from 'react'
 
 export function ScheduleForm() {
-  return <CalendarStep />
-  // return <ConfirmStep />
+  const [selectedDateTime, setSelectedDateTime] = useState<Date | null>()
+
+  function handleClearSelectedDateTime() {
+    setSelectedDateTime(null)
+  }
+
+  if (selectedDateTime) {
+    return (
+      <ConfirmStep
+        schedulingDate={selectedDateTime}
+        onCancelConfirmation={handleClearSelectedDateTime}
+      />
+    )
+  }
+
+  return <CalendarStep onSelectDateTime={setSelectedDateTime} />
 }
